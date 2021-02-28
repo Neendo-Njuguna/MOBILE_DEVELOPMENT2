e297da6d2e9ad507f73c2f270060f9178e91ee74
Neendo_Njuguna <njugunaneendo@gmail.com>
2021-02-28 13:20:40 +0300

    Asignment 2

---
+++ src/Assignment 2.kt
@@ -0,0 +1,29 @@
+fun main() {
+name()
+ modulus()
+ sum()
+ fact()
+
+}
+fun name(){
+ var name= "hello carole"
+ print(name)
+}
+fun modulus(){
+ var num1=35
+ var num2=11
+ var modulus=num1%num2
+ print(modulus)
+}
+fun sum(){
+ var num1=35
+ var num2=11
+ var num3=22
+ var num4=45
+ var sum= num1+num2+num3+num4
+ print (sum)
+}
+fun fact(){
+ var fact= "i love listening to music"
+ print(fact)
+}
